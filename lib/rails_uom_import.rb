798693c9cc6be588d011ddce8d50999176b1cb98
nicolasrsande <nicolasrsande@gmail.com>
2020-05-27 23:23:16 -0300

    add require

--- lib/rails_uom_import.rb
+++ lib/rails_uom_import.rb
@@ -1,4 +1,8 @@
 require "rails_uom_import/version"
+require 'fixy'
+require 'rails_uom_import/fixy/formatter/numeric'
+require 'rails_uom_import/records/employee_data'
+require 'rails_uom_import/documents/uom_import'
 
 module RailsUomImport
   class Error < StandardError; end
