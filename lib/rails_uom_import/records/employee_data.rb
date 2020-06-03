9976ab024c60f4640bccef4deca4943049fa96cb
nicolasrsande <nicolasrsande@gmail.com>
2020-06-03 15:20:35 -0300

    merge new fields

--- lib/rails_uom_import/records/employee_data.rb
+++ lib/rails_uom_import/records/employee_data.rb
@@ -4,7 +4,7 @@ module RailsUomImport
     include Fixy::Formatter::Numeric
     include Fixy::Formatter::Alphanumeric
 
-    set_record_length 149
+    set_record_length 151
 
     # Fields Declaration:
     # -----------------------------------------------------------
@@ -12,20 +12,24 @@ module RailsUomImport
     # ------------------------------------------------------------
     field :cuil, 11, '1-11', :numeric
     field :nombre, 30, '12-41', :alphanumeric
-    field :filler, 88, '42-129', :alphanumeric
+    field :provincia, 2, '104-105', :numeric
+    field :estado_civil, 2, '110-111', :numeric
     field :situacion, 2, '130-131', :numeric
-    field :filler2, 8, '132-139', :alphanumeric
+    field :incapacidad, 1, '135-135', :numeric
     field :sindicato, 1, '140-140', :alphanumeric
-    field :filler3, 1, '141-141', :alphanumeric
     field :sueldo, 8, '142-149', :alphanumeric
+    field :categoria, 2, '150-151', :numeric
 
     def initialize(record)
       @cuil = record[:cuil]
       @nombre = record[:nombre]
-      @filler = @filler2 = @filler3 = ' '
+      @provincia = record[:provincia] || 01 # Default "CABA"
+      @estado_civil = record[:estado_civil] || 1 # Default "SOLTERO"
       @situacion = record[:situacion] || 00 # Default "Recibe haberes regularmente"
       @sindicato = record[:sindicato] || 'S' # Default "Si"
       @sueldo = record[:sueldo]
+      @categoria = record[:categoria] || 1 # Default "Operario"
+      @incapacidad = record[:incapacidad] || 'N' # Default "No"
       define_attr_readers
     end
 
